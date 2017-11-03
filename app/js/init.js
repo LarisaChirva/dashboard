6fcd2e8beb746e650f5ff40f47b83dd9c28ca9bc
LarisaChirva <larisa87chirva@gmail.com>
2017-11-03 16:51:08 +0300

    Сделала работу по занятию

--- app/js/init.js
+++ app/js/init.js
@@ -39,10 +39,48 @@ $( function() {
 
         e.preventDefault();
         var target = $(this).attr('data-target');
-        $(target).bPopup();
+        $(target).bPopup({
+            closeClass: 'close'
+        });
 
     })
 
 //    form element stayler init
     $('select').styler();
+
+    $('.scrollbar-inner').scrollbar();
+
+//    chart init
+    var chartBlock = document.getElementById('myChart');
+    if( chartBlock ){
+        var myChart = new Chart(chartBlock, {
+            type: 'bar',
+            data:{
+                labels: ['01', '02','03','04','05','06','07','08','09'],
+                datasets: [
+                    {
+                        label: 'Signups',
+                        data: [200, 100, 210, 100, 30, 370, 200, 130, 50],
+                        backgroundColor: 'rgba(128, 191, 202, 1)'
+                    },
+                    {
+                        label: 'FTD',
+                        data: [100, 140, 10, 380, 130, 300, 290, 120, 250],
+                        backgroundColor: 'rgba(255, 154, 56, 1)'
+                    }
+                ]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        })
+    }
 } );
+
